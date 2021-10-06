6ae1065b976b366d9fe4a3b94d5ddf59bca3de4a
boy-min <sonhyunmin00@naver.com>
2021-10-07 01:26:42 +0900

    init commit

---
+++ multi-table.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+argc=${#}
+
+if [ $argc -ne 2 ]; then
+	echo -e "ERROR : WRONG ARGUMENTS NUMBER"
+	exit 1
+fi
+
+row=${1}
+col=${2}
+
+if [ $row -lt 1 -o $col -lt 1 ]; then
+	echo -e "ERROR : WRONG ARGUMENTS RANGE"
+	exit 1
+fi
+
+for ((i=1; i<=$row; i++)); do
+	for ((j=1; j<=$col; j++)); do
+		result=`expr $i \* $j`
+		echo -n "$i*$j=$result "
+	done
+	echo
+done
